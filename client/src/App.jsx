e776ea0c2defec9b6cdd71e004667d38caead6ff
Marc Nelson B. Ochavo <89735506+n3wt0n03@users.noreply.github.com>
2023-11-08 09:00:22 +0800

    revised client folder as React Vite

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import LandingPage from './components/index'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles/index.css'
+
+function index() {
+  return (
+    <div>
+        <div className="nav-container">
+            <img id="search-btn" src="src/assets/search.png" alt="search button.png" />
+            <h1> LOGO </h1>
+            <button id="login-btn"> LOGIN </button>
+        </div>
+
+        <div className='banner'>
+            <h1>BANNER</h1>
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default index
