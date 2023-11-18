394c7d646967e651fbfb1c0985c990f3e76aab12
T-jay A. Abunales <tjabunales@gmail.com>
2023-11-18 21:23:47 +0800

    added jwt and handle errors to auth

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,46 @@ const db = require('../models');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const maxAge = 3 * 24 * 60 *60;
+const createToken = (id) => {
+  return jwt.sign({ id }, 'Apartelle Secret Website', {
+    expiresIn: maxAge
+  })
+}
+
+function handleErrors(error) {
+  let errors = { email: '', password: '', contact_number: '', first_name: '', last_name: '' };
+
+  // Handling duplicate email or contact number
+  if (error.name === 'SequelizeUniqueConstraintError') {
+    if (error.errors && error.errors.length > 0) {
+      error.errors.forEach((err) => {
+        if (err.path === 'email') {
+          errors.email = 'Email already registered';
+        }
+        if (err.path === 'contact_number') {
+          errors.contact_number = 'Contact number already registered';
+        }
+      });
+    }
+    return { status: 409, errors };
+  }
+
+  // Handling validation errors
+  if (error.name === 'SequelizeValidationError') {
+    error.errors.forEach(({ path, message }) => {
+      if (errors.hasOwnProperty(path)) {
+        errors[path] = message;
+      }
+    });
+    return { status: 400, errors };
+  }
+
+  // Other errors
+  return { status: 500, errors: { message: 'Internal Server Error' } };
+}
+
+
 const register = async (req, res) => {
   try {
     // Hash password
@@ -16,10 +56,12 @@ const register = async (req, res) => {
       contact_number: req.body.contact_number,
       user_type: 'User' // Or based on what you receive in req.body
     });
-
-    res.status(201).send({ message: 'User created successfully', user });
+    const token = createToken(user.user_id);
+    res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000})
+    res.status(201).send({ message: 'User created successfully'});
   } catch (error) {
-    res.status(500).send({ message: 'Error creating user', error });
+    const { status, errors } = handleErrors(error);
+    res.status(status).send({ errors });
   }
 };
 
@@ -38,14 +80,13 @@ const login = async (req, res) => {
     }
 
     // Generate token
-    const token = jwt.sign({ id: user.user_id }, process.env.JWT_SECRET, {
-      expiresIn: 86400 // 24 hours
-    });
-
-    res.status(200).send({ message: 'Login successful', token });
+    const token = createToken(user.user_id);
+    
+    res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000})
+    res.status(200).send({ message: 'Login successful'});
   } catch (error) {
-    console.log(error)
-    res.status(500).send({ message: 'Error logging in', error });
+    const { status, errors } = handleErrors(error);
+    res.status(status).send({ errors });
   }
 };
 
