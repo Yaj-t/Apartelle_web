3d3bd4a02790a60d60b5b06a8fb33a52c0ab9bf6
Bryce Nicholas B. Co <21102461@usc.edu.ph>
2023-11-19 22:55:47 +0800

    Need help uwu

---
+++ client/src/AccountSettings.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./AccountSettings.css";
+
+const AccountSettings = () => {
+  return (
+    <div className="container">
+      <h1 className="header">Account Settings</h1>
+      <h2 className="subheader">Organize your stay and experience</h2>
+      <div className="button-container">
+        <Link to="/PersonalDetails">
+          <button>
+            <img src="/PersonalDetail.png" alt="Personal Detail" />
+            <div>
+              <h3>Personal Details</h3>
+              <p>Edit your personal information</p>
+            </div>
+          </button>
+        </Link>
+        <button>
+          <img src="/Security.png" alt="Security" />
+          <div>
+            <h3>Security</h3>
+            <p>Manage your account security</p>
+          </div>
+        </button>
+        <button>
+          <img src="/Booking.png" alt="Booking" />
+          <div>
+            <h3>Booking</h3>
+            <p>View and manage your bookings</p>
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AccountSettings;
---
+++ client/src/PersonalDetails.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PersonalDetails = () => {
+  return (
+    <div>
+      <h1>Personal Details Page</h1>
+      <p>This is the content of the Personal Details page.</p>
+    </div>
+  );
+};
+
+export default PersonalDetails;
