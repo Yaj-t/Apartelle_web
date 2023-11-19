b1e27f509fedd2e25407e615bed4acf8cfb032b6
T-jay A. Abunales <tjabunales@gmail.com>
2023-11-19 13:45:18 +0800

    added auth and required auth to user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const { requireAuth } = require('../middleware/authMiddleware');
+const { requireAuth, authRole } = require('../middleware/authMiddleware');
 const { User } = require("../models");
 
-router.get('/users', requireAuth, async (req, res) => {
+router.get('/users', authRole(['ADMIN', 'Employee']), async (req, res) => {
   try {
       // Fetch all users from the database
       const users = await User.findAll({
@@ -23,7 +23,7 @@ router.get('/users', requireAuth, async (req, res) => {
 });
 
 
-router.get('/profile/:userId', async (req, res) => {
+router.get('/profile/:userId', authRole(['ADMIN', 'Employee']), async (req, res) => {
     try {
       const user = await User.findByPk(req.params.userId);
       if (!user) {
@@ -47,7 +47,7 @@ router.get('/profile/:userId', async (req, res) => {
     }
   });
 
-router.put('/profile/:userId', async (req, res) => {
+router.put('/profile/:userId', authRole(['ADMIN']), async (req, res) => {
     try {
       req.body.password = await bcrypt.hash(req.body.password, 10);
       const user = await User.findByPk(req.params.userId);
@@ -63,7 +63,7 @@ router.put('/profile/:userId', async (req, res) => {
     }
   });
 
-router.delete('/profile/:userId', async (req, res) => {
+router.delete('/profile/:userId', authRole(['ADMIN']), async (req, res) => {
     try {
       const user = await User.findByPk(req.params.userId);
       if (!user) {
@@ -77,7 +77,74 @@ router.delete('/profile/:userId', async (req, res) => {
     }
   });
   
-  
+router.get('/myprofile', requireAuth, async (req, res) => {
+  try {
+    const token = req.cookies.jwt;
+    const decodedToken = jwt.verify(token, 'Apartelle Secret Website');
+    const user = await User.findByPk(decodedToken.id);
+
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+
+    res.status(200).json({
+      message: 'Profile fetched successfully',
+      user: {
+        id: user.user_id,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        user_type: user.user_type,
+        contact_number: user.contact_number
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Internal server error' });
+  }
+});
+
+// PUT /myprofile - Update own profile
+router.put('/myprofile', requireAuth, async (req, res) => {
+  try {
+    const token = req.cookies.jwt;
+    const decodedToken = jwt.verify(token, 'Apartelle Secret Website');
+    const user = await User.findByPk(decodedToken.id);
+
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+
+    if (req.body.password) {
+      req.body.password = await bcrypt.hash(req.body.password, 10);
+    }
+
+    await user.update(req.body);
+    res.status(200).send({ message: 'Profile updated successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Internal server error' });
+  }
+});
+
+// DELETE /myprofile - Delete own profile
+router.delete('/myprofile', requireAuth, async (req, res) => {
+  try {
+    const token = req.cookies.jwt;
+    const decodedToken = jwt.verify(token, 'Apartelle Secret Website');
+    const user = await User.findByPk(decodedToken.id);
+
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+
+    await user.destroy();
+    res.status(200).send({ message: 'Profile deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Internal server error' });
+  }
+});
   
 
 module.exports = router
