492de86d711137b86fa511275bb04154ce55a16d
T-jay A. Abunales <tjabunales@gmail.com>
2023-11-24 22:59:54 +0800

    some fixes after merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,8 @@ import PersonnelsAdmin from './components/admin/PersonnelsAdmin'
 import PersonnelDetailsAdmin from './components/admin/PersonnelDetailsAdmin'
 import MainPageAdmin from './components/admin/MainPageAdmin'
 import NavBarOnline from './components/NavBars/NavBarOnline'
+import AddRoomType from './components/roomType/addRoomType'
+import ShowRoomTypes from './components/roomType/showRoomTypes'
 import './App.css'
 
 
@@ -22,6 +24,10 @@ function App() {
 
   return (
     <>
+    <Routes>
+      <Route path='/addRoomType' element={<AddRoomType/>}/>
+      <Route path='/showRoomTypes' element={<ShowRoomTypes/>}/>
+    </Routes>
     {/* Uncomment this when using the user page */}
     <Routes>
       <Route path='/' element={<LandingPage />} />
