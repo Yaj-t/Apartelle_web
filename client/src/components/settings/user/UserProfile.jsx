1d61dad93f12f00df5edb5325841c61a160cf2f3
Bryce Nicholas B. Co <21102461@usc.edu.ph>
2023-12-06 22:31:43 +0800

    profile changes

--- client/src/components/settings/user/UserProfile.jsx
+++ client/src/components/settings/user/UserProfile.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
-import { Link } from 'react-router-dom';
-import { Card } from '@mui/material';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import UserNavBar from '../../NavBars/UserNavBar';
-import SettingsSidebar from '../../NavBars/SettingsSidebar';
-import Footer from '../../Footer';
-import UserCSS from '../../../styles/settings/userProfile.module.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { Link } from "react-router-dom";
+// import { Card } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import UserNavBar from "../../NavBars/UserNavBar";
+import SettingsSidebar from "../../NavBars/SettingsSidebar";
+import Footer from "../../Footer";
+import UserCSS from "../../../styles/settings/userProfile.module.css";
 
 const ProfilePage = () => {
   const [profile, setProfile] = useState(null);
@@ -17,9 +17,9 @@ const ProfilePage = () => {
     const fetchProfile = async () => {
       try {
         const response = await Axios.get(
-          'http://localhost:3001/user/myprofile/',
+          "http://localhost:3001/user/myprofile/",
           {
-            headers: { accessToken: sessionStorage.getItem('accessToken') }
+            headers: { accessToken: sessionStorage.getItem("accessToken") },
           }
         );
         const profileData = response.data.user;
@@ -37,7 +37,7 @@ const ProfilePage = () => {
   }
 
   return (
-    <div className='profile-page'>
+    <div className="profile-page">
       <UserNavBar />
 
       <div className={UserCSS.profileContainer}>
@@ -50,7 +50,7 @@ const ProfilePage = () => {
           </div>
 
           <div className={UserCSS.userInfoContainer}>
-            <div className={UserCSS.photoDetails}>
+            {/* <div className={UserCSS.photoDetails}>
               <Card className={UserCSS.userPhotoCard}>
                 <div className={UserCSS.userPhoto}>
                   <AccountCircleIcon id={UserCSS.userPhotoIcon} />
@@ -59,46 +59,26 @@ const ProfilePage = () => {
               </Card>
 
               <div id={UserCSS.editProfile}>
-                <Link to='update'>
+                <Link to="update">
                   <button>Edit Profile</button>
                 </Link>
               </div>
-            </div>
+            </div> */}
 
             <div className={UserCSS.userInfoDetails}>
               <div className={UserCSS.userInfoHeader}>
                 <h1>
                   Hello, {profile.firstName} {profile.lastName}
                 </h1>
-                <p>Joined in </p>
+                {/* <p>Joined in </p> */}
               </div>
 
-              <div className={UserCSS.userAbout}>
-                <h3> About </h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Beatae itaque odit tempora exercitationem quod magnam fuga
-                  tenetur nobis aliquam, libero doloribus voluptates consequatur
-                  eveniet, eaque dolores similique iusto eius corrupti.
-                </p>
-              </div>
-
-              <div className={UserCSS.userAddresstoAge}>
+              {/* <div className={UserCSS.userAddresstoAge}>
                 <div className={UserCSS.userContainer}>
                   <h3>Address</h3>
                   <p>Purok 7, Bunga-Mar, Jagna, Bohol</p>
                 </div>
-
-                <div className={UserCSS.userContainer}>
-                  <h3>Date of Birth</h3>
-                  <p>12/03/2003</p>
-                </div>
-
-                <div className={UserCSS.userContainer}>
-                  <h3> Age </h3>
-                  <p>20</p>
-                </div>
-              </div>
+              </div> */}
 
               <div className={UserCSS.userEmailtoPass}>
                 <p>
@@ -111,15 +91,20 @@ const ProfilePage = () => {
                   <p>
                     <b> Password: </b>
                   </p>
-                  <VisibilityIcon />
-                  <VisibilityOffIcon />
+                  <p>**********</p>
+                  {/* <VisibilityIcon />
+                  <VisibilityOffIcon /> */}
                 </div>
               </div>
             </div>
           </div>
+          <div id={UserCSS.editProfile}>
+            <Link to="update">
+              <button>Edit Profile</button>
+            </Link>
+          </div>
         </div>
       </div>
-
       <Footer />
     </div>
   );
