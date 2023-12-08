149fd329d0725fd49efa668993757f980fe85938
Bryce Nicholas B. Co <21102461@usc.edu.ph>
2023-12-09 03:04:01 +0800

    ReservationsSettings can get data from the database. Its not fully finished due to dummy data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,80 +1,84 @@
-import { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import LandingPage from './components';
-import RoomDetails from './components/RoomDetails';
-import AllRooms from './components/AllRooms';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import ReservationAdmin from './components/admin/Reservation/ReservationAdmin';
-import ReservationDetails from './components/admin/Reservation/ReservationDetails';
-import RoomsAdmin from './components/admin/Rooms/AddRooms/RoomsAdmin';
-import RoomsFormAdmin from './components/admin/Rooms/AddRooms/RoomsFormAdmin';
-import RoomsAllAdmin from './components/admin/Rooms/AddRooms/RoomsAllAdmin';
-import RoomsDetailsAdmin from './components/admin/Rooms/AddRooms/RoomsDetailsAdmin';
-import PersonnelsAdmin from './components/admin/Personnel/PersonnelsAdmin';
-import PersonnelDetailsAdmin from './components/admin/Personnel/PersonnelDetailsAdmin';
-import MainPageAdmin from './components/admin/MainPageAdmin';
-import EditRoomType from './components/admin/Rooms/AddRoomTypes/RoomEditType';
-import RoomTypeForm from './components/admin/Rooms/AddRoomTypes/RoomsAddTypeAdmin';
-import RoomTypes from './components/admin/Rooms/AddRoomTypes/RoomsAllTypesAdmin';
-import UserProfile from './components/settings/user/UserProfile';
-import UpdateUserProfile from './components/settings/user/UpdateProfile';
-import './App.css';
-import AccountSettings from './components/settings/AccountSettings';
-import Security from './components/settings/SecuritySettings';
-import ReservationsSettings from './components/settings/ReservationsSettings';
-import PersonalDetails from './PersonalDetails';
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./components";
+import RoomDetails from "./components/RoomDetails";
+import AllRooms from "./components/AllRooms";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import ReservationAdmin from "./components/admin/Reservation/ReservationAdmin";
+import ReservationDetails from "./components/admin/Reservation/ReservationDetails";
+import RoomsAdmin from "./components/admin/Rooms/AddRooms/RoomsAdmin";
+import RoomsFormAdmin from "./components/admin/Rooms/AddRooms/RoomsFormAdmin";
+import RoomsAllAdmin from "./components/admin/Rooms/AddRooms/RoomsAllAdmin";
+import RoomsDetailsAdmin from "./components/admin/Rooms/AddRooms/RoomsDetailsAdmin";
+import PersonnelsAdmin from "./components/admin/Personnel/PersonnelsAdmin";
+import PersonnelDetailsAdmin from "./components/admin/Personnel/PersonnelDetailsAdmin";
+import MainPageAdmin from "./components/admin/MainPageAdmin";
+import EditRoomType from "./components/admin/Rooms/AddRoomTypes/RoomEditType";
+import RoomTypeForm from "./components/admin/Rooms/AddRoomTypes/RoomsAddTypeAdmin";
+import RoomTypes from "./components/admin/Rooms/AddRoomTypes/RoomsAllTypesAdmin";
+import UserProfile from "./components/settings/user/UserProfile";
+import UpdateUserProfile from "./components/settings/user/UpdateProfile";
+import "./App.css";
+import AccountSettings from "./components/settings/AccountSettings";
+import Security from "./components/settings/SecuritySettings";
+import ReservationsSettings from "./components/settings/ReservationsSettings";
+import PersonalDetails from "./PersonalDetails";
 // import RoomDetail from './components/01test/RoomDetail
-import ProtectedRoutes from './components/ProtectedRoute';
-import UnauthorizeError from './components/errorpages/UnauthorizeError';
-import AvailableRooms from './components/01test/AvailableRooms';
-import BookRoom from './components/01test/BookRoom';
-
+import ProtectedRoutes from "./components/ProtectedRoute";
+import UnauthorizeError from "./components/errorpages/UnauthorizeError";
+import AvailableRooms from "./components/01test/AvailableRooms";
+import BookRoom from "./components/01test/BookRoom";
+import UserBookings from "./components/01test/UserBookings";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/available-rooms' element={<AvailableRooms></AvailableRooms>}/>  
-        <Route path='/book-room/:roomId' element={<BookRoom></BookRoom>}></Route>
+        <Route
+          path="/available-rooms"
+          element={<AvailableRooms></AvailableRooms>}
+        />
+        <Route
+          path="/book-room/:roomId"
+          element={<BookRoom></BookRoom>}
+        ></Route>
+        <Route path="/user-bookings" element={<UserBookings></UserBookings>} />
       </Routes>
-      
-
-     
 
       {/* For the admin index modules */}
       <Routes>
         <Route element={<ProtectedRoutes />}>
-          <Route path='/admin' element={<MainPageAdmin />} />
-          <Route path='/admin/reservation' element={<ReservationAdmin />} />
-          <Route path='/admin/rooms' element={<RoomsAdmin />} />
-          <Route path='/admin/personnel' element={<PersonnelsAdmin />} />
+          <Route path="/admin" element={<MainPageAdmin />} />
+          <Route path="/admin/reservation" element={<ReservationAdmin />} />
+          <Route path="/admin/rooms" element={<RoomsAdmin />} />
+          <Route path="/admin/personnel" element={<PersonnelsAdmin />} />
 
           {/* For the admin room type modules */}
           <Route
-            path='/admin/rooms/showRoomTypes/editRoomType/:id'
+            path="/admin/rooms/showRoomTypes/editRoomType/:id"
             element={<EditRoomType />}
           />
-          <Route path='/admin/rooms/addRoomType' element={<RoomTypeForm />} />
-          <Route path='/admin/rooms/showRoomTypes' element={<RoomTypes />} />
+          <Route path="/admin/rooms/addRoomType" element={<RoomTypeForm />} />
+          <Route path="/admin/rooms/showRoomTypes" element={<RoomTypes />} />
 
           {/* For the admin rooms modules */}
-          <Route path='/admin/rooms/addRooms' element={<RoomsFormAdmin />} />
-          <Route path='/admin/rooms/showAllRooms' element={<RoomsAllAdmin />} />
+          <Route path="/admin/rooms/addRooms" element={<RoomsFormAdmin />} />
+          <Route path="/admin/rooms/showAllRooms" element={<RoomsAllAdmin />} />
           <Route
-            path='/admin/rooms/showAllRooms/roomDetails'
+            path="/admin/rooms/showAllRooms/roomDetails"
             element={<RoomsDetailsAdmin />}
           />
 
           {/* For the admin reservation modules */}
           <Route
-            path='/admin/reservation/details'
+            path="/admin/reservation/details"
             element={<ReservationDetails />}
           />
 
           {/* For the admin personnel modules */}
           <Route
-            path='/admin/personnel/personnelDetails'
+            path="/admin/personnel/personnelDetails"
             element={<PersonnelDetailsAdmin />}
           />
         </Route>
@@ -82,32 +86,32 @@ function App() {
 
       {/* For the user modules */}
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/allRooms' element={<AllRooms />} />
-        <Route path='/roomDetails/:roomId' element={<RoomDetails />} />
-        
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/allRooms" element={<AllRooms />} />
+        <Route path="/roomDetails/:roomId" element={<RoomDetails />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
 
       {/* For the settings module */}
       <Routes>
-        <Route path='/settings' element={<AccountSettings />} />
-        <Route path='/settings/security' element={<Security />} />
-        <Route path='/settings/manage-account' element={<UserProfile />} />
+        <Route path="/settings" element={<AccountSettings />} />
+        <Route path="/settings/security" element={<Security />} />
+        <Route path="/settings/manage-account" element={<UserProfile />} />
         <Route
-          path='/settings/manage-account/update'
+          path="/settings/manage-account/update"
           element={<UpdateUserProfile />}
         />
         {/* userprofile in progress*/}
         <Route
-          path='/settings/reservation'
+          path="/settings/reservation"
           element={<ReservationsSettings />}
         />
       </Routes>
 
       <Routes>
-        <Route path='/unauthorized' element={<UnauthorizeError />} />
+        <Route path="/unauthorized" element={<UnauthorizeError />} />
       </Routes>
 
       {/* For the user profile modules */}
--- client/src/components/settings/ReservationsSettings.jsx
+++ client/src/components/settings/ReservationsSettings.jsx
@@ -1,14 +1,69 @@
-import React from 'react';
-import SettingsSidebar from '../NavBars/SettingsSidebar';
-import UserNavBar from '../NavBars/UserNavBar';
-import Footer from '../Footer';
-import { Card, CardMedia } from '@mui/material';
-import { Link } from 'react-router-dom';
-import ReserveSettingsCSS from '../../styles/settings/reservationSettings.module.css';
-import UserBookings from '../01test/UserBookings'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SettingsSidebar from "../NavBars/SettingsSidebar";
+import UserNavBar from "../NavBars/UserNavBar";
+import Footer from "../Footer";
+import {
+  Card,
+  CardMedia,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button,
+} from "@mui/material";
+// import { useParams } from "react-router-dom";
+import ReserveSettingsCSS from "../../styles/settings/reservationSettings.module.css";
+import UserBookings from "../01test/UserBookings";
+import picture from "../../assets/Room_Picture.jpg";
 
-import picture from '../../assets/Room_Picture.jpg'
 const ReservationsSettings = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDetailsModalOpen, setIsDetailsModalOpen] = useState(false);
+  const [bookingData, setBookingData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchRoomData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/booking/my-bookings`,
+          {
+            headers: { accessToken: sessionStorage.getItem("accessToken") },
+          }
+        );
+        console.log(response.data);
+        setBookingData(response.data);
+      } catch (err) {
+        setError("Room not found or an error occurred.");
+        console.error(err);
+      }
+    };
+
+    fetchRoomData();
+  }, []);
+
+  const handleCancelClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleConfirmCancel = () => {
+    setBookingCancelled(true);
+    setIsModalOpen(false);
+  };
+
+  const handleViewDetailsClick = () => {
+    setIsDetailsModalOpen(true);
+  };
+
+  const handleCloseDetailsModal = () => {
+    setIsDetailsModalOpen(false);
+  };
+
   return (
     <div>
       <UserNavBar />
@@ -21,44 +76,108 @@ const ReservationsSettings = () => {
             Track your bookings here
           </h2>
           <hr className={ReserveSettingsCSS.dividerBookings} />
-          {/* Add more content related to bookings here */}
-            {/* <CardMedia  
-              sx={{ height: 200 }}
-              image={picture} 
-              title='room picture'
-            /> */}
           <Card>
-            <div className={ReserveSettingsCSS['card-container']}>
-              <CardMedia  
-                sx={{ width: 180, height: 120 }}
-                image={picture} 
-                title='room picture'
+            <div className={ReserveSettingsCSS["card-container"]}>
+              <CardMedia
+                className={ReserveSettingsCSS["room-picture"]}
+                image={picture}
+                title="room picture"
               />
-              <div className={ReserveSettingsCSS['room-booking']} >
-                <div className={ReserveSettingsCSS['room-details']}>
-                  <h4 className={ReserveSettingsCSS['text']}>Fully Furnished Apartment</h4>
-                  <div className={ReserveSettingsCSS['details']}>
-                    <div className={ReserveSettingsCSS['para']}>
-                      <p>Check In: <span className={ReserveSettingsCSS['blurey']}>12 Mar 2021</span></p>
+              <div className={ReserveSettingsCSS["room-booking"]}>
+                <div className={ReserveSettingsCSS["room-details"]}>
+                  <h4 className={ReserveSettingsCSS["text"]}>
+                    Fully Furnished Apartment
+                  </h4>
+                  <div className={ReserveSettingsCSS["details"]}>
+                    <div className={ReserveSettingsCSS["para"]}>
+                      <p>
+                        Check In:{" "}
+                        <span className={ReserveSettingsCSS["blurey"]}>
+                          12 Mar 2021
+                        </span>
+                      </p>
                     </div>
-                    <div className={ReserveSettingsCSS['para']}>
-                      <p>Duration: <span className={ReserveSettingsCSS['blurey']}>Long ( 2 - 5 Years )</span> </p>
+                    <div className={ReserveSettingsCSS["para"]}>
+                      <p>
+                        Duration:{" "}
+                        <span className={ReserveSettingsCSS["blurey"]}>
+                          Long ( 2 - 5 Years )
+                        </span>{" "}
+                      </p>
                     </div>
-                  <div className={ReserveSettingsCSS['para']}>
-                    <p>Guests: <span className={ReserveSettingsCSS['blurey']}>4 Adults</span></p>
+                    <div className={ReserveSettingsCSS["para"]}>
+                      <p>
+                        Guests:{" "}
+                        <span className={ReserveSettingsCSS["blurey"]}>
+                          4 Adults
+                        </span>
+                      </p>
+                    </div>
                   </div>
-                  </div>
-                  <h4 className={ReserveSettingsCSS['text']}>$ 1000 USD</h4>
+                  <h4 className={ReserveSettingsCSS["text"]}>$ 1000 USD</h4>
                 </div>
-
               </div>
-              <div className={ReserveSettingsCSS['button-pos']}>
-                  <button className={ReserveSettingsCSS['button1']}>Cancel Reservation</button>
-                  <button className={ReserveSettingsCSS['button2']}>View reserve</button>
-                    </div>
+
+              <div className={ReserveSettingsCSS.ViewDetails}>
+                <button
+                  className={ReserveSettingsCSS["button2"]}
+                  onClick={handleViewDetailsClick}
+                >
+                  View Details
+                </button>
+                <Dialog
+                  open={isDetailsModalOpen}
+                  onClose={handleCloseDetailsModal}
+                >
+                  <DialogTitle>Booking Details</DialogTitle>
+                  <DialogContent>
+                    {bookingData ? (
+                      <div>
+                        {/* Display the fetched data */}
+                        <p>Room ID: {bookingData.roomId}</p>
+                        <p>Check-in Date: {bookingData.checkInDate}</p>
+                        {/* Add other properties as needed */}
+                      </div>
+                    ) : (
+                      <p>No data available</p>
+                    )}
+                  </DialogContent>
+                </Dialog>
+              </div>
+
+              <div className={ReserveSettingsCSS["button-pos"]}>
+                <button
+                  className={ReserveSettingsCSS["button1"]}
+                  onClick={handleCancelClick}
+                >
+                  Cancel Reservation
+                </button>
+                <Dialog open={isModalOpen} onClose={handleCloseModal}>
+                  <DialogTitle className={ReserveSettingsCSS.DialogTitle}>
+                    You are about to cancel your booking.
+                  </DialogTitle>
+                  <DialogContent className={ReserveSettingsCSS.dialogContent}>
+                    <p>Are you sure you want to cancel?</p>
+                  </DialogContent>
+
+                  <div className={ReserveSettingsCSS.bothModalButtons}>
+                    <button
+                      onClick={handleConfirmCancel}
+                      className={ReserveSettingsCSS.modalButton2}
+                    >
+                      Proceed to Cancel
+                    </button>
+                    <button
+                      onClick={handleCloseModal}
+                      className={ReserveSettingsCSS.modalButton1}
+                    >
+                      I change my mind
+                    </button>
+                  </div>
+                </Dialog>
+              </div>
             </div>
           </Card>
-
         </div>
       </div>
       <Footer />
